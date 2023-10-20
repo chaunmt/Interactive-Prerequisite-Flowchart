8ec198ebb9ae078a27ce4414facef11078f7ed4d
Su Yeon Han <78541890+shan3462@users.noreply.github.com>
2023-10-20 17:10:58 -0500

    Search.js - real

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,14 +1,40 @@
-import { access } from "../Routes";
+import React, { useState, useEffect } from 'react';
 
-const Search = {
-  coursesList,
+function SearchBar() {
+  const [data, setData] = useState([]); //list of items from JSON
+  const [search, setSearch] = useState(''); //Current search term
+
+  useEffect(() => { //Fetching data
+    fetch('CSCI.json')
+      .then(response => response.json())
+      .then(data => {
+        setData(data.class);
+      });
+  }, []);
+
+  const handleSearch = (event) => { //Filtering data
+    setSearch(event.target.value);
+  };
+
+  const filteredData = data.filter(item => {
+    return item.name.toLowerCase().includes(search.toLowerCase()) || 
+           item.end.some(code => (`CSCI ${code}`).toLowerCase().includes(search.toLowerCase()));
+  });
+
+  return ( //Display 
+    <div className="SearchBar">
+      <input type="text" placeholder="Search" onChange={handleSearch} />
+      <ul>
+        {filteredData.length > 0 ? 
+          filteredData.map((item, index) => (
+            <li key={index}> {item.end[0]} - {item.name}</li>
+          ))
+          :
+          <li>No search results</li>
+        }
+      </ul>
+    </div>
+  );
 }
 
-function coursesList(input) {
-  // search courses by input: by name and id
-  // return array of course name and id that match input
-}
-
-
-
-export default Search;
+export default SearchBar;
