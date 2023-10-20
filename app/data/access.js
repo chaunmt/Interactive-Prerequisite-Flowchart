31ade04ea20b32ec6da4ff85b10b7a3c400a7b6d
Junminkyu <mkjun0911@gmail.com>
2023-10-20 16:10:49 -0500

    Created findCourseInfo function
    Created findCourseInfo function.
    
    If you put the course ID (ex CSCI 1933) into the function, the function will return the array of a prerequisite

--- app/data/access.js
+++ app/data/access.js
@@ -1,5 +1,21 @@
-const access = {
+const sample = require('./CSCI.json');
 
+
+
+const access = {
+  findCourseInfo
 }
+
+function findCourseInfo(input){
+    for(var i=0;sample.class.length;i++){
+        if(input==sample.class[i].end){
+            return sample.class[i].start;
+        }
+    }
+}
+
+// this is for testing, you can erase if you want
+var arr=findCourseInfo('CSCI 1933');
+console.log(arr);
 
 export default access;
