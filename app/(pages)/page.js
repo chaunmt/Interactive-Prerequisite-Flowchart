31a9687869bf9f50f5e6fd1fbff5e5f640589d44
doggu <someoneyoumayknow0@gmail.com`>
2023-10-21 16:22:46 -0500

    use search and make CSCI.json a public file

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Search from "../components/Search"
 
 export default function Page() {
   return (
     <div>
       <h1>Welcome to "Interactive" Prerequisites</h1>
+      <Search />
       <Link href="/csci"><button>CSCI</button></Link>
     </div>
   );
