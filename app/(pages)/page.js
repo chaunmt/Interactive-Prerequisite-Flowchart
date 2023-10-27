5de09fdcda470624fdc233e02f0555a11a3227ab
doggu <someoneyoumayknow0@gmail.com>
2023-10-26 23:44:18 -0500

    make graph use search

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -1,27 +1,15 @@
 import Link from "next/link";
-import Search from "../components/Search"
-import Mermaid from "../components/Mermaid"
+import GraphSearch from "../components/GraphSearch";
+
+import mermaid from "mermaid";
 
 export default function Page() {
-  const graph = `
-  graph TD
-  CSCI_1933[CSCI 1933]
-  CSCI_2011[CSCI 2011]
-  CSCI_2041[CSCI 2041]
-  CSCI_4041[CSCI 4041]
-  CSCI_4011[CSCI 4011]
-  CSCI_1933 --> CSCI_2041
-  CSCI_2011 --> CSCI_2041 & CSCI_4041
-  CSCI_1933 --> CSCI_4041
-  CSCI_2041 --> CSCI_4011 --> something
-  `
   return (
     <div>
       <h1>Welcome to "Interactive" Prerequisites</h1>
       <Link href="/csci"><button>All CSCI Classes</button></Link>
-      {/* ERROR: graph does not auto reload on changes */}
-      <Mermaid graph = {graph}/>
-      <Search />
+      <br/>
+      <GraphSearch />
     </div>
   );
 }
