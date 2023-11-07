8d0ee4d56638724ec41a30bdfb78c417905ee352
Junminkyu <mkjun0911@gmail.com>
2023-11-06 19:57:27 -0600

    Footer,header

---
+++ app/components/Footer.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function Footer(){
+    return <div className="layout-footer">Made by Social Coding
+                <div>
+                    <Link href='https://github.com/chaunmt/Interactive-Prerequisite-Flowchart' target="_blank">
+                        Contribute to Github
+                    </Link>  
+                </div>
+            </div>
+            
+}
+
+export default Footer
