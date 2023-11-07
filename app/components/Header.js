81e519edbea6ac95cca7808f0804073431d7f799
Junminkyu <mkjun0911@gmail.com>
2023-11-06 20:59:03 -0600

    Header update

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
 function Header(){
-    return <div className='layout-header'>Gophers Prerequisites</div>
+    return <div className="header">
+                <nav><img src='../photo/umn.png' alt="umn_logo" width="300"/>
+                <ul>
+                    <li><a href='#'>About</a></li>
+                    <li><a href='#'>Q&A</a></li>
+                    <li><a href='https://github.com/chaunmt/Interactive-Prerequisite-Flowchart' target="_blank">Contribute to Github</a></li>
+                </ul>
+            </nav>
+        </div>
+   
 }
 
 export default Header
