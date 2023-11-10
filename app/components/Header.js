c395b3ec0b201de90075d736bee61ef830786776
Junminkyu <mkjun0911@gmail.com>
2023-11-09 20:03:37 -0600

    Update

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,10 +6,11 @@ import logo from 'public/logo.png';
 function Header(){
     return <div className="header">
                 <nav> 
-                    <img src="https://i.ibb.co/ZBLJYqL/Gophers-1-2.png" alt="umn_prerequisite_logo"/>
+                    <a href='../'><img src="https://i.ibb.co/ZBLJYqL/Gophers-1-2.png" alt="umn_prerequisite_logo"/></a>
                 <ul>
                     <li><a href='#'>About</a></li>
                     <li><a href='#'>Q&A</a></li>
+                    <li><a href='/csci'>Course Table</a></li>
                     <li><a href='https://github.com/chaunmt/Interactive-Prerequisite-Flowchart' target="_blank">Contribute to Github</a></li>
                 </ul>
             </nav>
