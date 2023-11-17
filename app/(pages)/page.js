64575035e50e2116f309537e41fafab10acc750e
KongmengT <124010307+Sin-ner0@users.noreply.github.com>
2023-11-16 19:59:05 -0600

    Update

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -1,18 +1,40 @@
-import Link from "next/link";
-import GraphSearch from "../components/GraphSearch";
-import mermaid from "mermaid";
+// import Link from "next/link";
+// import GraphSearch from "../components/GraphSearch";
+// import mermaid from "mermaid";
+// import '../components/styles/Layout.css';
+// import '../components/styles/Mainpage.css';
+// import Footer from '../components/Footer.js';
+// import Header from '../components/Header.js';
+
+// export default function Page() {
+//   return (
+//     <div>
+//       <Header/>
+//       <br/> {/* this line break doesn't seem to do anything */}
+//       <GraphSearch/>
+//       <Footer/>
+//     </div>
+//   );
+
+"use client"
+
+import CoursesTable from "../components/CoursesTable";
 import '../components/styles/Layout.css';
 import '../components/styles/Mainpage.css';
 import Footer from '../components/Footer.js';
 import Header from '../components/Header.js';
 
-export default function Page() {
+//TODO: fix or update
+export default function CSCI() {
   return (
-    <div>
+    <div id="page">
+       
       <Header/>
-      <br/> {/* this line break doesn't seem to do anything */}
-      <GraphSearch/>
+      <div id="backBoard">
+      <h1 id="title">CSCI COURSES</h1>
+      <CoursesTable />
+      </div>
       <Footer/>
-    </div>
+      </div>
   );
 }
