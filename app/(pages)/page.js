8ba54b77fd9c580c874c005e7c534b4c6943c7c4
Junminkyu <mkjun0911@gmail.com>
2023-11-30 18:54:55 -0600

    Almost final

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -1,40 +1,18 @@
-// import Link from "next/link";
-// import GraphSearch from "../components/GraphSearch";
-// import mermaid from "mermaid";
-// import '../components/styles/Layout.css';
-// import '../components/styles/Mainpage.css';
-// import Footer from '../components/Footer.js';
-// import Header from '../components/Header.js';
-
-// export default function Page() {
-//   return (
-//     <div>
-//       <Header/>
-//       <br/> {/* this line break doesn't seem to do anything */}
-//       <GraphSearch/>
-//       <Footer/>
-//     </div>
-//   );
-
-"use client"
-
-import CoursesTable from "../components/CoursesTable";
+import Link from "next/link";
+import GraphSearch from "../components/GraphSearch";
+import mermaid from "mermaid";
 import '../components/styles/Layout.css';
 import '../components/styles/Mainpage.css';
 import Footer from '../components/Footer.js';
 import Header from '../components/Header.js';
 
-//TODO: fix or update
-export default function CSCI() {
+export default function Page() {
   return (
-    <div id="page">
-       
+    <div>
       <Header/>
-      <div id="backBoard">
-      <h1 id="title">CSCI COURSES</h1>
-      <CoursesTable />
-      </div>
+      <br/> {/* this line break doesn't seem to do anything */}
+      <GraphSearch />
       <Footer/>
-      </div>
+    </div>
   );
 }
---
+++ app/(pages)/table/page.js
@@ -0,0 +1,40 @@
+// import Link from "next/link";
+// import GraphSearch from "../components/GraphSearch";
+// import mermaid from "mermaid";
+// import '../components/styles/Layout.css';
+// import '../components/styles/Mainpage.css';
+// import Footer from '../components/Footer.js';
+// import Header from '../components/Header.js';
+
+// export default function Page() {
+//   return (
+//     <div>
+//       <Header/>
+//       <br/> {/* this line break doesn't seem to do anything */}
+//       <GraphSearch/>
+//       <Footer/>
+//     </div>
+//   );
+
+"use client"
+
+import CoursesTable from "../../components/CoursesTable";
+import '../../components/styles/Layout.css';
+import '../../components/styles/Mainpage.css';
+import Footer from '../../components/Footer.js';
+import Header from '../../components/Header.js';
+
+//TODO: fix or update
+export default function CSCI() {
+  return (
+    <div id="page">
+       
+      <Header/>
+      <div id="backBoard">
+      <h1 id="title">CSCI COURSES</h1>
+      <CoursesTable />
+      </div>
+      <Footer/>
+      </div>
+  );
+}
