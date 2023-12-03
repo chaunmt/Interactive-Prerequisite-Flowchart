2ebb9180f96aaae112cbdf6deb10bbc973e1f961
cocochanel0516 <148506522+cocochanel0516@users.noreply.github.com>
2023-12-03 01:53:33 -0600

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,17 +5,18 @@ import logo from 'public/logo.png';
 
 function Header(){
     return <div className="header">
-                <nav> 
-                    <a href='../'><img src="https://i.ibb.co/ZBLJYqL/Gophers-1-2.png" alt="umn_prerequisite_logo"/></a>
+                <nav>    
                 <ul>
                     <li><a href='#'>About</a></li>
                     <li><a href='#'>Q&A</a></li>
                     <li><a href='./table'>Course Table</a></li>
                     <li><a href='https://github.com/chaunmt/Interactive-Prerequisite-Flowchart' target="_blank">Contribute to Github</a></li>
+                <a href='../'><img src="https://i.ibb.co/ZBLJYqL/Gophers-1-2.png" alt="umn_prerequisite_logo"/></a>
                 </ul>
-            </nav>
-        </div>
-   
+                </nav>
+            </div>
 }
+
+
 
 export default Header
