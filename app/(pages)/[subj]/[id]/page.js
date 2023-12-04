b6a983baf91467040c5ffaf6c9e97618e93190ed
cocochanel0516 <148506522+cocochanel0516@users.noreply.github.com>
2023-12-04 10:10:57 -0600

    Update page.js

--- app/(pages)/[subj]/[id]/page.js
+++ app/(pages)/[subj]/[id]/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Mermaid from "../../../components/Mermaid";
 import {buildGraph} from "../../../data/graphBuilder";
+import logo from 'public/logo.png';
 
 
 export default function Page({ params }) {
@@ -10,8 +11,9 @@ export default function Page({ params }) {
   
   return (
     <div>
+      <a href='../'><img src="https://i.ibb.co/ZBLJYqL/Gophers-1-2.png" alt="umn_prerequisite_logo"/></a>
       <h1>You are on the {subj} {id} page.</h1>
-      <Link href="/"><button>Homepage</button></Link>
+       {/* <Link href="/"><button>Homepage</button></Link> */}
       <Link href={`/${params.subj}`}><button>{subj} Page</button></Link>
       <Link href="./table"><button>{subj} Table</button></Link>
       <Mermaid graph={test}/>
