13af9a8cee82d67619fe035934728fc2becb0578
Chau Truong <37993386+chaunmt@users.noreply.github.com>
2024-02-01 14:49:24 -0600

    collapse chosen node's childrens

--- app/components/ReaGraph.js
+++ app/components/ReaGraph.js
@@ -1,7 +1,8 @@
 "use client";
 
 import React from 'react';
-import { useState } from 'react';
+import { useCollapse } from 'reagraph';
+import { getVisibleEntities } from 'reagraph';
 import { GraphCanvas } from 'reagraph';
 import "./styles/ReaGraph.css"
 
@@ -28,18 +29,13 @@ const nodes = [
   }
 ];
 
+// collapse can only work with 1 way
 const edges = [
   {
     source: '1',
     target: '2',
     id: '1-2',
     label: '1-2'
-  },
-  {
-    source: '2',
-    target: '1',
-    id: '2-1',
-    label: '2-1'
   },
   {
     source: '2',
@@ -58,6 +54,25 @@ const edges = [
 export const MyReaGraph = () => {
   const [active, setActive] = React.useState(null);
   const [collapsed, setCollapsed] = React.useState(['n-2']);
+  const {
+    getExpandPathIds
+  } = useCollapse({
+    collapsedNodeIds: collapsed,
+    nodes: nodes,
+    edges: edges
+  });
+  const hiddenNodeIds = React.useMemo(() => {
+    const {
+      visibleNodes
+    } = getVisibleEntities({
+      collapsedIds: collapsed,
+      nodes: nodes,
+      edges: edges
+    });
+    const visibleNodeIds = visibleNodes.map(n => n.id);
+    const hiddenNodes = nodes.filter(n => !visibleNodeIds.includes(n.id));
+    return hiddenNodes.map(n => n.id);
+  }, [collapsed]);
   return <div style={{
     position: 'absolute',
     top: 0,
@@ -65,7 +80,7 @@ export const MyReaGraph = () => {
     left: 0,
     right: 0
   }}>
-      <div style={{
+    <div style={{
       zIndex: 9,
       position: 'absolute',
       top: 15,
@@ -74,11 +89,11 @@ export const MyReaGraph = () => {
       padding: 10,
       color: 'white'
     }}>
-        <h3>Node Actions</h3>
-        {active ? <>
-            Selected: {active.node.id}
-            <br />
-            <button style={{
+      <h3>Node Actions</h3>
+      {active ? <>
+          Selected: {active.node.id}
+          <br />
+          <button style={{
           display: 'block',
           width: '100%'
         }} onClick={() => {
@@ -86,9 +101,9 @@ export const MyReaGraph = () => {
             setCollapsed([...collapsed, active.node.id]);
           }
         }}>
-              Collapse Node
-            </button>
-            <button style={{
+            Collapse Node
+          </button>
+          <button style={{
           display: 'block',
           width: '100%'
         }} onClick={() => {
@@ -96,19 +111,35 @@ export const MyReaGraph = () => {
             setCollapsed(collapsed.filter(n => n !== active.node.id));
           }
         }}>
-              Expand Node
+            Expand Node
+          </button>
+        </> : <>
+          Click a node to see options
+        </>}
+      <h3>Collapsed Nodes</h3>
+      <code>
+        <pre>
+          {JSON.stringify(collapsed, null, 2)}
+        </pre>
+      </code>
+      <h3>Hidden Nodes</h3>
+      <ul>
+        {hiddenNodeIds.map(id => <li key={id}>
+            {id}
+            <button style={{
+            display: 'block',
+            width: '100%'
+          }} onClick={() => {
+            const toExpandIds = getExpandPathIds(id);
+            const newCollapsed = collapsed.filter(id => !toExpandIds.includes(id));
+            setCollapsed(newCollapsed);
+          }}>
+              View Node
             </button>
-          </> : <>
-            Click a node to see options
-          </>}
-        <h3>Collapsed Nodes</h3>
-        <code>
-          <pre>
-            {JSON.stringify(collapsed, null, 2)}
-          </pre>
-        </code>
-      </div>
-      <GraphCanvas collapsedNodeIds={collapsed} nodes={nodes} edges={edges} onNodeClick={(node, props) => setActive({
+          </li>)}
+      </ul>
+    </div>
+    <GraphCanvas collapsedNodeIds={collapsed} nodes={nodes} edges={edges} onNodeClick={(node, props) => setActive({
       node,
       props
     })} />
