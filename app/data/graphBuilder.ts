bd887507d55a01ec5c7cc4e5723a1097d8e2461d
jahndan <sankar.chandan@gmail.com>
2024-03-08 15:33:07 -0600

    improved debug for graphbuilder

--- app/data/graphBuilder.ts
+++ app/data/graphBuilder.ts
@@ -8,8 +8,16 @@ import {
 } from "./types";
 // import { NodeData, EdgeData } from "reagraph"; // "reaflow"
 
-/** turns on/off debug messages from buildGraph */
-let debug: boolean = true;
+/** debug messages from buildGraph */
+const debug: {
+  readonly process?: boolean; // print at each stage of the process in order of evaluation
+  readonly output?: boolean; // print the output graph in dual list form before returning
+  readonly reduced?: boolean; // compacts the log output at the expense of some specificity
+} = {
+  // process: true,
+  // output: true,
+  reduced: true,
+};
 
 /** pass in either a courseshell or an array of courseshells, and get back the appropriate graph */
 export default function buildGraph(input: CourseShell | CourseShell[]) {
@@ -32,11 +40,9 @@ function build(courses: CourseShell[]): {
   if (courses.length == 0) {
     console.log("WARNING: no courses passed in, built empty graph.");
   }
-  if (debug) {
-    console.log(
-      "buildgraph:",
-      courses.map((x) => x.code),
-    );
+  if (debug.process) {
+    const log = debug.reduced ? courses.map((x) => x.code) : courses;
+    console.log("begin build:", log);
   }
 
   let node_list: NodeData[] = [];
@@ -53,30 +59,35 @@ function build(courses: CourseShell[]): {
     andx,
   ); // no return value needed, so most of the lambdas can be void
 
-  courses.forEach((course) => {
-    if (debug) {
-      console.log(">", course.code);
+  courses.forEach((course, i) => {
+    if (debug.process) {
+      const log = debug.reduced ? course.code : course;
+      console.log("list:", log, "—", i);
     }
-    single(course);
+    singleton(course);
   });
 
-  if (debug) {
-    console.log("end buildgraph:", node_list, edge_list);
+  if (debug.output) {
+    const nlog = debug.reduced ? node_list.map((n) => n.id) : node_list;
+    const elog = debug.reduced
+      ? edge_list.map((e) => `${e.from} -> ${e.to}`)
+      : edge_list;
+    console.log("finish build:", nlog, elog);
   }
   return {
     nodes: node_list,
     edges: edge_list,
   };
 
-  function single(course: CourseShell) {
-    if (debug) {
-      console.log("single:", course.code);
+  function singleton(course: CourseShell) {
+    if (debug.process) {
+      const log = debug.reduced ? course.code : course;
+      console.log("singleton:", log);
     }
     // request new accessor only when necessary
     if (!accessors.has(course.subject)) {
       accessors.set(course.subject, Access(course.subject));
     }
-
     let sublevel = Number(course.id.match(/\d+/g)[0]) < 1000;
     const full_course = sublevel
       ? {
@@ -86,15 +97,15 @@ function build(courses: CourseShell[]): {
           prereq: [],
         }
       : accessors.get(course.subject).get(course);
-    // if the course is not found in Access, simply do nothing
+    // TODO proper error handling
     if (full_course == null) {
       console.log(
         "ERROR: Invalid course found while building graph:",
         course.code,
       );
+      // throw Error(`Invalid course found: ${course.code}`);
       return;
     }
-
     const { code, subject, id, prereq } = full_course;
     let node_id = `${subject}_${id}`;
     // skip if already processed
@@ -106,7 +117,7 @@ function build(courses: CourseShell[]): {
   }
 
   function course_lambda(preq: CourseShell, state: build_state) {
-    let node_id = single(preq);
+    let node_id = singleton(preq);
     let edge_id = `${node_id}___${state.nid}`;
     if (edge_list.every((edge) => edge.id !== edge_id)) {
       edge_list.push({ id: edge_id, from: node_id, to: state.nid });
