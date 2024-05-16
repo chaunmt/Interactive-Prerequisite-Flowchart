7ef0b5a69672d38bd4f886c617c9863bd5a0046d
Steven Tam <steven.tam123@gmail.com>
2024-05-15 22:23:49 -0500

    branch test

--- app/(pages)/editor/page.js
+++ app/(pages)/editor/page.js
@@ -47,6 +47,7 @@ export default function Page({ params }) {
     setSearch(event.target.value);
   };
 
+  console.log("major-minor branch test")
   return (
     <div id="editor">
       <div id="resultGraph">
