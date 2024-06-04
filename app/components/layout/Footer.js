ef906e567eaa3ca0444773af36bcdf66727fc779
Steven Tam <steven.tam123@gmail.com>
2024-06-03 22:03:33 -0500

    Added Contact Us

--- app/components/layout/Footer.js
+++ app/components/layout/Footer.js
@@ -6,12 +6,17 @@ export default function Footer() {
     <div id="footer">
       <h2 id="clubName">© 2023 Social Coding</h2>
 
+      
       <Link
         id="github"
         href="https://github.com/chaunmt/Interactive-Prerequisite-Flowchart"
         target="_blank" // Open in new tab
       >
         <strong>Contribute to Github</strong>
+      </Link>
+
+      <Link href="mailto:tam00024@umn.edu">
+        <h4>Contact Us</h4>
       </Link>
     </div>
   );
