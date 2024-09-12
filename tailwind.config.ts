e22282047a513dd7ec6179d02d69a33efc5f8097
Chau Truong <chauminh.0210@gmail.com>
2024-09-11 20:45:25 -0500

    add nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,15 @@
-import type { Config } from 'tailwindcss'
- 
+import { nextui } from "@nextui-org/theme";
+import type { Config } from "tailwindcss";
+
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "// Note the addition of the app directory.\n    ./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-export default config
+  plugins: [nextui()],
+};
+export default config;
