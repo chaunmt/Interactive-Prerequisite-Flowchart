63e49a09d6c174bd8baba474d392955d3adb8ce1
Chau Truong <chauminh.0210@gmail.com>
2024-09-11 20:39:38 -0500

    tailwind example (temp)

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+ 
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+export default config
