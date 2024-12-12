a395dd28e87b25f26304ea48cb27620766c57045
jahndan <jahndandot@gmail.com>
2024-12-11 21:02:10 -0600

    dynamic css styling lol

--- components/graph/Mermaid.tsx
+++ components/graph/Mermaid.tsx
@@ -3,8 +3,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import mermaid, { MermaidConfig } from "mermaid";
 import elkLayouts from "@mermaid-js/layout-elk";
-
-import "../styles/Mermaid.css";
+import { reformat } from "@/backend/text-manipulation.tsx";
 
 /***
 STYLING:
@@ -46,8 +45,13 @@ mermaid.initialize(siteConfig);
 
 let { dx, dy, std, opacity } = { dx: 0.4, dy: 0.8, std: 2.0, opacity: 0.15 };
 const nodeshadows = `<filter id="nodeshadows"><feDropShadow dx="${dx}" dy="${dy}" stdDeviation="${std}" flood-opacity="${opacity}"></feDropShadow></filter>`;
-({ dx, dy, std, opacity } = { dx: 0.2, dy: 0.4, std: 0.8, opacity: 0.4 });
+({ dx, dy, std, opacity } = { dx: 0.15, dy: 0.3, std: 0.6, opacity: 0.5 });
 const allshadows = `<filter id="allshadows"><feDropShadow dx="${dx}" dy="${dy}" stdDeviation="${std}" flood-opacity="${opacity}"></feDropShadow></filter>`;
+
+const courseStyles = "fill-sky-300"// dark:fill-sky-800 dark:text-white";
+const andStyles = "fill-green-300"// dark:fill-green-800 dark:text-white";
+const orStyles = "fill-pink-300"// dark:fill-pink-800 dark:text-white";
+const restyles = [courseStyles, orStyles, andStyles].join(" ");
 
 export default function Mermaid({ graph }) {
   const [didMount, setDidMount] = useState(false);
@@ -64,20 +68,16 @@ export default function Mermaid({ graph }) {
 
       // extraneous DOM manipulation to add additional styling to mermaid output
       const parser = new DOMParser();
-      const svgdoc = parser.parseFromString(`<div>${svg}</div>`, "text/xml");
+      const svgdoc = parser.parseFromString(`<div id="top">${svg}</div>`, "text/xml");
       const topsvg = svgdoc.getElementsByTagName("svg")[0];
 
       // making svg filter available
       topsvg.innerHTML = allshadows + nodeshadows + topsvg.innerHTML;
 
-      // // shadows don't work on these (edges won't render)
-      // const edges = topsvg.getElementsByClassName("edges")[0];
-      // edges.setAttribute("filter", "url(#shadowTag)");
-
-      // applying small drop shadow to everything instead
+      // applying small drop shadow to everything
       topsvg.setAttribute("filter", "url(#allshadows)");
 
-      // applying drop shadow and slightly rounded corners to nodes
+      // applying drop shadow and slightly rounded corners to course nodes
       const nodes = topsvg.getElementsByClassName("nodes")[0];
       nodes.setAttribute("filter", "url(#nodeshadows)");
       const rects = Array.from(nodes.getElementsByTagName("rect"));
@@ -87,8 +87,31 @@ export default function Mermaid({ graph }) {
         }
       });
 
+      // stripping mermaid styles and manually styling each node type
+      const ors = Array.from(nodes.getElementsByClassName("OR"));
+      ors.forEach(o => {
+        o.classList.remove("node", "default");
+        orStyles.split(/\s+/).forEach(css => {
+          o.classList.add(css);
+        });
+      })
+      const ands = Array.from(nodes.getElementsByClassName("AND"));
+      ands.forEach(a => {
+        a.classList.remove("node", "default");
+        andStyles.split(/\s+/).forEach(css => {
+          a.classList.add(css);
+        });
+      })
+      const courses = Array.from(nodes.getElementsByClassName("default"));
+      courses.forEach(c => {
+        c.classList.remove("node");
+        courseStyles.split(/\s+/).forEach(css => {
+          c.classList.add(css);
+        });
+      })
+
       // getting it back into a string
-      const topdiv = svgdoc.getElementsByTagName("div")[0];
+      const topdiv = svgdoc.getElementById("top");
       const fixedsvg = topdiv.innerHTML;
 
       // updating the real page DOM
@@ -102,5 +125,9 @@ export default function Mermaid({ graph }) {
     setDidMount(true);
   }, []);
 
-  return <div className="mermaid">{graph}</div>;
+  return <div className="mermaid">
+    <pre className="text-xs/none">{reformat(graph)}</pre>
+    {/* TODO ensure appropriate Tailwind classes are loaded in production */}
+    {/* <div className={restyles}></div> */}
+  </div>;
 }
